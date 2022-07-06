3e031d8868ace0e09c40529541169b570b42e7ba
MOISES MARTINEZ CID <20183l301006@utcv.edu.mx>
2022-07-06 12:48:53 -0500

    Respaldo del api en el repositorio

--- routes/api.php
+++ routes/api.php
@@ -23,25 +23,25 @@ use App\Http\Middleware\JwtMiddleware;
 
 Route::post('login', [UsuariosController::class, 'authenticate']);
 
-Route::get('users/{id}', [UsuariosController::class, 'editar']);
-Route::put('users/{id}', [UsuariosController::class, 'updated']);
-Route::delete('users/{id}', [UsuariosController::class, 'delete']);
-Route::post('upload', [UsuariosController::class, 'upload']);
-Route::post('admin', [UsuariosController::class, 'registerAdmin']);
 Route::post('users', [UsuariosController::class, 'register']);
 
-Route::get('users', [UsuariosController::class, 'list']);
-
-Route::get('posts/{id}', [PostsController::class, 'editar']);
-Route::put('posts/{id}', [PostsController::class, 'updated']);
-Route::put('activar/{id}', [PostsController::class, 'activar']);
-Route::post('posts/upload', [PostsController::class, 'upload']);
-Route::post('posts', [PostsController::class, 'create']);
-Route::get('posts', [PostsController::class, 'listactivos']);
-Route::get('postsInactivo', [PostsController::class, 'listaInactivos']);
-Route::delete('posts/{id}', [PostsController::class, 'updated']);
 
 Route::group(['middleware' => ['jwt.verify']], function () {
-
+    Route::get('users/{id}', [UsuariosController::class, 'editar']);
+    Route::put('users/{id}', [UsuariosController::class, 'updated']);
+    Route::delete('users/{id}', [UsuariosController::class, 'delete']);
+    Route::post('upload', [UsuariosController::class, 'upload']);
+    Route::post('admin', [UsuariosController::class, 'registerAdmin']);
+    
+    Route::get('users', [UsuariosController::class, 'list']);
+    
+    Route::get('posts/{id}', [PostsController::class, 'editar']);
+    Route::put('posts/{id}', [PostsController::class, 'updated']);
+    Route::put('activar/{id}', [PostsController::class, 'activar']);
+    Route::post('posts/upload', [PostsController::class, 'upload']);
+    Route::post('posts', [PostsController::class, 'create']);
+    Route::get('posts', [PostsController::class, 'listactivos']);
+    Route::get('postsInactivo', [PostsController::class, 'listaInactivos']);
+    Route::delete('posts/{id}', [PostsController::class, 'updated']);
 
 });
